1388e69446e9a800fe931b255a40d03174b95419
Hans Arthur Cupiterson <KetsunoAna117@users.noreply.github.com>
2024-04-16 20:16:10 +0700

    Making TabView

--- Notes/View/ContentView.swift
+++ Notes/View/ContentView.swift
@@ -9,13 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     @EnvironmentObject private var noteList: NoteList
+    @State private var activeTab = Tab.mynotes
+    
+    // make enum
+    enum Tab {
+        case mynotes
+        case myprofile
+    }
     
     var body: some View {
         NavigationStack {
             VStack(alignment: .leading) {
-                MyNotesView()
-                    .environmentObject(noteList)
-//                Profile(user: noteList.user)
+                // kondisi view berdasarkan tab
+                TabView(selection: $activeTab, content:  {
+                    switch activeTab {
+                        case .mynotes:
+                            MyNotesView()
+                                .tag(Tab.mynotes)
+                        case .myprofile:
+                            Profile(user: noteList.user)
+                                .tag(Tab.myprofile)
+                        }
+                })
+
             }
         }
     }
