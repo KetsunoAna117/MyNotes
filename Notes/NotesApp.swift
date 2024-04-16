201f3e50916daf72974a43fde4d41c8b7a4e3441
Hans Arthur Cupiterson <KetsunoAna117@users.noreply.github.com>
2024-04-16 17:23:07 +0700

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Hans Arthur Cupiterson on 16/04/24.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
