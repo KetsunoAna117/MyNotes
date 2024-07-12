0abda080bddcceee39b980b25cd891b906e03c7b
Hans Arthur Cupiterson <hansarthur20@gmail.com>
2024-07-12 12:03:06 +0700

    Initial Commit

---
+++ MyNotes/MyNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyNotesApp.swift
+//  MyNotes
+//
+//  Created by Hans Arthur Cupiterson on 12/07/24.
+//
+
+import SwiftUI
+
+@main
+struct MyNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
